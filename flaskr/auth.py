1d341dc9337776fd13ea64a3030b847d8bb69728
brged <asksa123@163.com>
2021-12-12 22:55:46 +0800

    模板

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -26,12 +26,12 @@ def register():
             error="Username is required."
         elif not password:
             error="Password is required."
-        elif db.execute("SELECT id FROM user WHERE username = ?", username).fetchone() is not None:
+        elif db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone() is not None:
             # 判断用户名是否存在
             error = "User {} is already registered.".format(username)
         # 通过校验
         if error is None:
-            db.execute("INSERT INTO user (username, password) VALUES (?, ?)", username, generate_password_hash(password))
+            db.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, generate_password_hash(password),))
             db.commit()
             return redirect(url_for("auth.login"))
         # 用于储存在渲染模块时可以调用的信息
@@ -55,7 +55,7 @@ def login():
         elif not password:
             error="Password is required."
         
-        user = db.execute("SELECT * FROM user WHERE username =?", username).fetchone()
+        user = db.execute("SELECT * FROM user WHERE username =?", (username,)).fetchone()
         # if user is None:
         #     error="Incorrect username."
         # elif not check_password_hash(user["password"], password):
@@ -67,6 +67,8 @@ def login():
             session.clear()
             session["user_id"]=user["id"]
             return redirect(url_for("index"))
+        
+        flash(error)
     
     return render_template("auth/login.html")
 
@@ -78,13 +80,13 @@ def load_logged_in_user():
     if user_id is None:
         g.user=None
     else:
-        g.user=get_db().execute("SELECT * FROM user WHERE id = ?", user_id).fetchone()
+        g.user=get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
 
 ## 注销
 @bp.route("/logout")
 def logout():
     session.clear()
-    redirect(url_for("index"))
+    return redirect(url_for("index"))
 
 # 用户登录以后才能创建、编辑和删除博客帖子。在每个视图中可以使用 装饰器 来完成这个工作。
 """装饰器返回一个新的视图，该视图包含了传递给装饰器的原视图。
